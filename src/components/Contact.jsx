db429b63c210e51382d6675673ee9e86a57a724c
Mohammad Habibullah <habibullah162470@gmail.com>
2024-11-18 23:12:26 +0600

    animated 3d earth and background of stars are done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className='xl:w-[500px] flex-[0.75] bg-black-100 p-8 rounded-2xl'
       >
         <p className={`${styles.sectionSubText}`}>Get in touch</p>
         <h3 className={`${styles.sectionHeadText}`}>Contact.</h3>
@@ -77,6 +77,12 @@ const Contact = () => {
             {loading ? "Sending..." : "Send"}
           </button>
         </form>
+      </motion.div>
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className='xl:flex-1 xs:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   )
